664bb483e0f9c372ead243b0e94a55af973fbd04
Altenor Gonçalves <altgsj@pop-os.localdomain>
2019-10-11 16:13:09 -0400

    more

--- src/components/Appointment/types.ts
+++ src/components/Appointment/types.ts
@@ -1,11 +1,20 @@
 import { Appointment } from "../../domain/Appointment.domain";
 
+type AppointmentActionType =
+  | "confirm-presence"
+  | "report-absense"
+  | "cancel-appointment"
+  | "reschedule-appointment"
+  | "schedule-return"
+  | "reverse-to-pending";
+
 export type AppointmentOuterProps = {
   appointment: Appointment;
+  today: Date;
+  onAction: (acfion: AppointmentActionType) => void;
 };
 
 export type AppointmentInnerProps = {
   menuAnchorElement: Element;
   setMenuAnchorElement: (evtTarget: Element) => void;
-  children: React.ReactNode;
 } & AppointmentOuterProps;
