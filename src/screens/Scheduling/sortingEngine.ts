1070b607f88e067bb8fb436e1f8948dfb5fbc36f
U-ACAD-UNN\mu04_labinfo4404 <mu04_labinfo4404@MU04-D028106.acad.uninorte.com.br>
2019-10-11 17:30:19 -0400

    Alteração do sortingEngine

--- src/screens/Scheduling/sortingEngine.ts
+++ src/screens/Scheduling/sortingEngine.ts
@@ -2,12 +2,31 @@ import { mapPropsStream } from "recompose";
 import { SchedulingInnerProps } from "./types";
 import { Observable } from "rxjs";
 import { Scheduling } from "./dumb";
+import { map, filter } from "rxjs/operators";
+import { combineLatest } from "rxjs/internal/observable/combineLatest";
 
 type TransformerFunction = (
   prop$: Observable<SchedulingInnerProps>
 ) => Observable<SchedulingInnerProps>;
 
-let transformer: TransformerFunction;
+const transformer: TransformerFunction = (prop$) => {
+  prop$.pipe(
+    map(({ appointments, filterMode, sortOrder, ...props }) => {
+      const sortedAppointments = appointments.filter(appointment => {
+        return true;
+      }).sort((appointmentA, appointmentB) => {
+        return 1;
+      });
+
+      return {
+        appointments: sortedAppointments,
+        filterMode,
+        sortOrder,
+        ...props
+      } as SchedulingInnerProps;
+    })
+  )
+};
 
 export const SortingEngine = mapPropsStream<
   SchedulingInnerProps,
