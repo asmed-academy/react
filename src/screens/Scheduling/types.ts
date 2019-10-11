e8c8644d429d75fc6d8fe68e8155d60d2d9d30bf
U-ACAD-UNN\19205031 <19205031@MU11-D035636.acad.uninorte.com.br>
2019-10-11 17:02:54 -0400

    Orders and Filters

--- src/screens/Scheduling/types.ts
+++ src/screens/Scheduling/types.ts
@@ -8,13 +8,13 @@ export type SchedulingOuterProps = {
 
 export type SortingOrder = "asc" | "desc";
 
+export type ChangeFilter = "future" | "backwards";
+
 export type SchedulingInnerProps = {
   menuAnchorElement: Element | null;
   setMenuAnchorElement: (anchorEl: Element | null) => void;
   sortOrder: SortingOrder;
   changeSortOrder: (sortOrder: SortingOrder) => void;
-  filterMode: "future" | "backwards";
-  changeFilterMode: (
-    filterMode: "future" | "backwards"
-  ) => void;
+  filterMode: ChangeFilter;
+  changeFilterMode: (filterMode: ChangeFilter) => void;
 } & SchedulingOuterProps;
