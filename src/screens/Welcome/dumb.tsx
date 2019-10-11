fd4f80f044a54c2c07884f28d46a557f00d316ad
livia128924 <55556787+livia128924@users.noreply.github.com>
2019-10-11 17:20:31 -0300

    alteraçao da indentificação

--- src/screens/Welcome/dumb.tsx
+++ src/screens/Welcome/dumb.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import { Button, TextField, AppBar, Typography, Box, CircularProgress } from "@material-ui/core";
+import Grid from '@material-ui/core/Grid';
+import { Button, TextField, AppBar, Typography, Box, CircularProgress, ButtonGroup } from "@material-ui/core";
 
-import { SearchRounded } from '@material-ui/icons';
+import { SearchRounded, FormatAlignJustify } from '@material-ui/icons';
+import { Layout } from "../../components/Layout/dumb";
 
 
 type WelcomeProps = {
@@ -19,46 +21,59 @@ export const Welcome = ({
   isLoadingData = false,
   cpfErrorText
 }: WelcomeProps) => (
-    <div>
-      <div style={{
-        display: 'flex',
-        width: '100%',
-        alignItems: 'flex-end'
-        , position: 'relative' 
-      }}>
-        <TextField
-          id="standard-name"
-          label="CPF"
-          margin="normal"
-          InputProps={{ fullWidth: true }}
-          style={{ width: '100%'}}
-          error={!!cpfErrorText} 
-          helperText={cpfErrorText}
-          onChange={evt => {
-            if (evt.target.value.length < 11) {
-              return;
-            }
-            onCPFReady(evt.target.value);
-          }}
-        />
-        {!isLoadingData && (
-          <SearchRounded
-            style={{
-              marginLeft: '16px',
-              position: 'relative',
-              bottom: '8px'
+    <Layout title="Identificação">
+      <React.Fragment >
+        <fieldset>
+        <ButtonGroup  style={{
+          display: 'flex',
+          width: '100%',
+          justifyContent: 'center',
+
+        }}>
+          <Button>Buscar por CPF</Button>
+          <Button>Ler QR Code</Button>
+        </ButtonGroup>
+        </fieldset>
+        <div style={{
+          display: 'flex',
+          width: '100%',
+          alignItems: 'flex-end'
+          , position: 'relative'
+        }}>
+          <TextField
+            id="standard-name"
+            label="CPF"
+            margin="normal"
+            InputProps={{ fullWidth: true }}
+            style={{ width: '100%' }}
+            error={!!cpfErrorText}
+            helperText={cpfErrorText}
+            onChange={evt => {
+              if (evt.target.value.length < 11) {
+                return;
+              }
+              onCPFReady(evt.target.value);
             }}
           />
-        )}
-        {
-          isLoadingData && (
-            <CircularProgress size={24} style={{
-              marginLeft: '16px',
-              position: 'relative',
-              bottom: '8px'
-            }} />
-          )
-        }
-      </div>
-    </div>
+          {!isLoadingData && (
+            <SearchRounded
+              style={{
+                marginLeft: '16px',
+                position: 'relative',
+                bottom: '8px'
+              }}
+            />
+          )}
+          {
+            isLoadingData && (
+              <CircularProgress size={24} style={{
+                marginLeft: '16px',
+                position: 'relative',
+                bottom: '8px'
+              }} />
+            )
+          }
+        </div>
+      </React.Fragment>
+    </Layout>
   );
