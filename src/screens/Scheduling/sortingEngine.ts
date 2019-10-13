feed41f009d2ec9aaff1357b4a6c3bd8fa14f05b
Henrique Souza <henriquesouza1929@gmail.com>
2019-10-12 22:08:19 -0400

    Melhorias no sortingEngine

--- src/screens/Scheduling/sortingEngine.ts
+++ src/screens/Scheduling/sortingEngine.ts
@@ -3,19 +3,42 @@ import { SchedulingInnerProps } from "./types";
 import { Observable } from "rxjs";
 import { Scheduling } from "./dumb";
 import { map, filter } from "rxjs/operators";
-import { combineLatest } from "rxjs/internal/observable/combineLatest";
 
 type TransformerFunction = (
   prop$: Observable<SchedulingInnerProps>
 ) => Observable<SchedulingInnerProps>;
 
 const transformer: TransformerFunction = (prop$) => {
-  prop$.pipe(
+  return prop$.pipe(
     map(({ appointments, filterMode, sortOrder, ...props }) => {
       const sortedAppointments = appointments.filter(appointment => {
-        return true;
+        const today = new Date();
+        if(filterMode === 'backwards' && appointment.date < today 
+        || filterMode === 'future' && appointment.date >= today){
+          return true;
+        }else{
+          return false;
+        }
+
       }).sort((appointmentA, appointmentB) => {
-        return 1;
+        if(sortOrder === 'asc'){
+          if(appointmentA.date > appointmentB.date){
+            return 1;
+          }else if(appointmentA < appointmentB){
+            return -1;
+          }else{
+            return 0;
+          }
+        }else{
+          if(appointmentA.date > appointmentB.date){
+            return -1;
+          }else if(appointmentA < appointmentB){
+            return 1;
+          }else{
+            return 0;
+          }
+        }
+        
       });
 
       return {
