4cacb642e1c9f00ccf5af990e9b4f881ffce821a
U-ACAD-UNN\mu04_labinfo4404 <mu04_labinfo4404@MU04-D028106.acad.uninorte.com.br>
2019-10-14 16:35:44 -0400

    Type user

---
+++ src/screens/AuthorizedUsers/sortingEngine.ts
@@ -0,0 +1,53 @@
+import { mapPropsStream } from "recompose";
+import { AuthorizedUsersInnerProps } from "./types";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+//import {} from './dumb';
+
+type TransformerFunction = (
+  prop$: Observable<AuthorizedUsersInnerProps>
+) => Observable<AuthorizedUsersInnerProps>;
+
+const transformer: TransformerFunction = prop$ => {
+  return prop$.pipe(
+    map(({ users, filterMode, sortOrder, ...props }) => {
+      const sortedUsers = users.filter(user => {
+
+        if (filterMode === 'active' && user.active || filterMode === 'inactive' && !user.active) {
+          return true;
+        } else {
+          return false;
+        }
+
+      }).sort((userA, userB) => {
+        if (sortOrder === 'asc') {
+          if (userA.name > userB.name) {
+            return -1;
+          } else if (userA.name < userB.name) {
+            return 1;
+          }
+          return 0;
+        } else {
+          if (userA.name > userB.name) {
+            return 1;
+          } else if (userA.name < userB.name) {
+            return -1;
+          }
+          return 0;
+        }
+      })
+
+      return {
+        users: sortedUsers,
+        filterMode,
+        sortOrder,
+        ...props
+      }
+    })
+  )
+};
+
+/*export const SortingEngine = mapPropsStream<
+  AuthorizedUsersInnerProps,
+  AuthorizedUsersInnerProps
+>(transformer)();*/
